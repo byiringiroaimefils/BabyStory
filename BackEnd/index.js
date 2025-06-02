04925878703c291ad3f7b6a505355cd19ee2df2e
BYIRINGIRO Aime Fils <123945889+byiringiroaimefils@users.noreply.github.com>
2025-06-02 16:11:48 +0200

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -14,7 +14,7 @@ const BiblicalSchema = require('./Modules/BiblicalSchema.js');
 
 //----MiddleWare configurations---
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:8000",
+  origin: process.env.FRONTEND_URL || "https://isomere.vercel.app",
   credentials: true,
   optionsSuccessStatus: 200
 }
